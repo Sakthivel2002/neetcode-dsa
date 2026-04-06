066fd8aae046f5d8af13686cb0d55890686b4dc4
Sakthivel P <sakthirollins175@gmail.com>
2026-04-06 22:33:46 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length()) return false;
+        int[] arr = new int[26];
+        for (char ch: s.toCharArray()){
+            arr[ch - 97]++;
+        }
+
+        for(char c:t.toCharArray()){
+            arr[c-97]--;
+        }
+        for(int i=0; i<26;i++){
+            if(arr[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
